d2f35d77e8a15994419973251795ee55b4264b20
tszching <30718976+tszching@users.noreply.github.com>
2018-04-19 17:21:43 +1000

    Xcode
    Xcode Project Created

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by CHLOE KWOK on 19/4/18.
+//  Copyright © 2018 CHLOE KWOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
