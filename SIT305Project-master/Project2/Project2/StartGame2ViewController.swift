ef339fa8ef2fcb67738d45f75238f31b05818f7b
tszching <30718976+tszching@users.noreply.github.com>
2018-04-20 14:49:50 +1000

    Animation Popup clicking
    Animation:
    -StartGame2ViewController (enemies popping up)
    -StartGame2ViewController (needs to be imported as SpriteKit and PlaygroundSupport)

--- SIT305Project-master/Project2/Project2/StartGame2ViewController.swift
+++ SIT305Project-master/Project2/Project2/StartGame2ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class StartGame2ViewController: UIViewController {
 
+    // Variables
+     var gameTimer : Timer?
+    var gravity : UIGravityBehavior?
+    var animator : UIDynamicAnimator?
     
-
+    
+ 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Set timer to show snowflaks
+        gameTimer = Timer.scheduledTimer(timeInterval: 1,
+                                         target: self,
+                                         selector: #selector(StartGame2ViewController.addenemy(_ :)), userInfo: nil, repeats: true)
+        
+        // Register an animator
+        animator = UIDynamicAnimator(referenceView: self.view)
+        gravity = UIGravityBehavior(items:[])
+        
+        //Gravity magnitude and direction
+        let vector = CGVector(dx: 0.0, dy: 0.1)
+        gravity?.gravityDirection = vector
+        animator?.addBehavior(gravity!)
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
+    
+    
+    @objc func addenemy ( _ : Any) {
+        
+        //Pick a random x position for the balloon
+        let xCoordinate = arc4random() % UInt32 (self.view.bounds.width)
+        
+        //Create a button, set Image, assign touchUpInside handler, add it to the view and gravity animator
+        
+        let btn = UIButton(frame: CGRect (x: Int(xCoordinate), y:60, width: 30, height : 30))
+        btn.setImage(UIImage(named: "enemy2"), for: .normal)
+        btn.addTarget(self, action: #selector(self.enemypopup(sender:)), for: .touchUpInside )
+        self.view.addSubview(btn)
+        
+        gravity?.addItem( btn as UIView)
+        
+    }
+    
+    @objc func enemypopup (sender : UIButton) {
+        
+        sender.setImage(UIImage (named : "enemy1"), for: .normal)
+        UIView.animate(withDuration: 0.4, animations:{sender.alpha = 0},
+                       completion: {(true) in sender.removeFromSuperview()})
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
